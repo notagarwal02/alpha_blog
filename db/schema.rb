2b3d575b6b26f39899da0f1dbeb1108af2a5cc89
Shubham Agarwal <shub.ag02@gmail.com>
2016-11-15 17:49:53 +0000

    create users table nad model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701131527) do
+ActiveRecord::Schema.define(version: 20161115173826) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "descriptions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
